692db5a905c4eee04887374be3b27da500be8ff3
oliverpardo1979 <125147979+oliverpardo1979@users.noreply.github.com>
2025-01-24 09:38:18 -0500

    Add files via upload

---
+++ Rcode/Auto.R
@@ -0,0 +1,6 @@
+library(dplyr)
+library(ggplot2)
+library(readxl)
+Auto <- read_excel("G:/Mi unidad/NewBox/Teaching/Javeriana/Analitica R/Data/Auto.xls")
+View(Auto)
+ggplot(Auto,aes(x=Horsepower,y=MPG)) + geom_point()
